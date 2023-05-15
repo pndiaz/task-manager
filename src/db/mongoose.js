244ecb358427611f73f92fe56036fe1eb82e4b5b
pndiaz <pablondiaz12@gmail.com>
2023-05-15 12:09:25 -0300

    Revert "async db connect"
    This reverts commit 84fdb13b95f861087daefb21db33c6044e2f9543.

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,13 +1,3 @@
 const mongoose = require('mongoose')
 
-const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGODB_URL);
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
-    } catch (error) {
-        console.log(error);
-        process.exit(1);
-    }
-}
-
-exports.connectDB = connectDB
+mongoose.connect(process.env.MONGODB_URL)
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const userRouter = require('./routers/user.js')
 const taskRouter = require('./routers/task.js')
-const { connectDB } = require('./db/mongoose')
 
 const app = express()
 const port = process.env.PORT
@@ -10,8 +9,6 @@ app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
-connectDB().then(() => {
-    app.listen(port, () => {
-        console.log("Running server on port " + port)
-    })
+app.listen(port, () => {
+    console.log("Running server on port " + port)
 })
