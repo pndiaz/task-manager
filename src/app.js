3a0c3b8545f21d451a74c83c30899fe7fc97ac62
pndiaz <pablondiaz12@gmail.com>
2023-08-17 16:53:59 -0300

    testing

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const userRouter = require('./routers/user.js')
+const taskRouter = require('./routers/task.js')
+
+const app = express()
+
+app.use(express.json())
+app.use(userRouter)
+app.use(taskRouter)
+
+module.exports = app
